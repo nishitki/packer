65ed4aa5c84742f764d0cc6f5a70c659541bbf5e
nishiki <sk8erdude@outlook.com>
2015-11-01 18:19:43 +0900

    update

---
+++ centos6/scripts/eth0.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cat <<EOF > /etc/sysconfig/network-scripts/ifcfg-eth0
+HWADDR=52:54:00:12:34:56
+DEVICE="eth0"
+BOOTPROTO="static"
+ONBOOT="yes"
+TYPE="Ethernet"
+IPADDR=172.16.64.55
+NETMASK=255.255.255.0
+GATEWAY=172.16.64.254
+EOF
